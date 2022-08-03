b2d70124eb96e4dd120995cbc2ee3866924a35f7
paleziart <paleziart@laas.fr>
2022-08-03 18:30:19 +0200

    Python bindings for new interface

--- cpuMLP/cpuMLP.cpp
+++ cpuMLP/cpuMLP.cpp
@@ -1,7 +1,7 @@
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
 #include <pybind11/eigen.h>
-#include "cpuMLP.hpp"
+#include "cMLP2.hpp"
 
 namespace py = pybind11;
 
@@ -26,5 +26,25 @@ PYBIND11_MODULE(stateEst3, m) {
     .def(py::init<>())
     .def("load", &MLP_2<123, 3>::updateParamFromTxt)
     .def("forward", &MLP_2<123, 3>::forward)
+    ;
+}
+
+PYBIND11_MODULE(interface, m) {
+  py::class_<cMLP2>(m, "Interface")
+    .def(py::init<>())
+    .def("initialize", &cMLP2::initialize)
+    .def("forward", &cMLP2::forward)
+    .def("update_observation", &cMLP2::update_observation)
+    .def_readwrite("obs_mean_", &cMLP2::obs_mean_)
+    .def_readwrite("obs_var_", &cMLP2::obs_var_)
+    .def_readwrite("P_", &cMLP2::P_)
+    .def_readwrite("D_", &cMLP2::D_)
+    .def_readwrite("pTarget12_", &cMLP2::pTarget12_)
+    .def_readwrite("q_init_", &cMLP2::q_init_)
+    .def_readwrite("vel_command_", &cMLP2::vel_command_)
+    .def_readwrite("state_est_obs_", &cMLP2::state_est_obs_)
+    .def_readwrite("obs_", &cMLP2::obs_)
+    .def_readwrite("bound_", &cMLP2::bound_)
+    .def_readwrite("bound_pi_", &cMLP2::bound_pi_)
     ;
 }
