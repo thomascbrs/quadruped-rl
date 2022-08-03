5f47ef29d54dec14844eeafe50e25924e581e7a9
paleziart <paleziart@laas.fr>
2022-08-03 16:45:18 +0200

    Continue working on C++ interface

--- cpuMLP/Types.h
+++ cpuMLP/Types.h
@@ -9,37 +9,39 @@
 #include <string>
 #include <memory>
 
-using Vector1 = Eigen::Matrix<double, 1, 1>;
-using Vector2 = Eigen::Matrix<double, 2, 1>;
-using Vector3 = Eigen::Matrix<double, 3, 1>;
-using Vector4 = Eigen::Matrix<double, 4, 1>;
-using Vector5 = Eigen::Matrix<double, 5, 1>;
-using Vector6 = Eigen::Matrix<double, 6, 1>;
-using Vector7 = Eigen::Matrix<double, 7, 1>;
-using Vector8 = Eigen::Matrix<double, 8, 1>;
-using Vector11 = Eigen::Matrix<double, 11, 1>;
-using Vector12 = Eigen::Matrix<double, 12, 1>;
-using Vector18 = Eigen::Matrix<double, 18, 1>;
-using Vector19 = Eigen::Matrix<double, 19, 1>;
-using Vector24 = Eigen::Matrix<double, 24, 1>;
-using VectorN = Eigen::Matrix<double, Eigen::Dynamic, 1>;
+using Vector1 = Eigen::Matrix<float, 1, 1>;
+using Vector2 = Eigen::Matrix<float, 2, 1>;
+using Vector3 = Eigen::Matrix<float, 3, 1>;
+using Vector4 = Eigen::Matrix<float, 4, 1>;
+using Vector5 = Eigen::Matrix<float, 5, 1>;
+using Vector6 = Eigen::Matrix<float, 6, 1>;
+using Vector7 = Eigen::Matrix<float, 7, 1>;
+using Vector8 = Eigen::Matrix<float, 8, 1>;
+using Vector11 = Eigen::Matrix<float, 11, 1>;
+using Vector12 = Eigen::Matrix<float, 12, 1>;
+using Vector18 = Eigen::Matrix<float, 18, 1>;
+using Vector19 = Eigen::Matrix<float, 19, 1>;
+using Vector24 = Eigen::Matrix<float, 24, 1>;
+using Vector123 = Eigen::Matrix<float, 123, 1>;
+using Vector132 = Eigen::Matrix<float, 132, 1>;
+using VectorN = Eigen::Matrix<float, Eigen::Dynamic, 1>;
 
-using Matrix2 = Eigen::Matrix<double, 2, 2>;
-using Matrix3 = Eigen::Matrix<double, 3, 3>;
-using Matrix4 = Eigen::Matrix<double, 4, 4>;
-using Matrix12 = Eigen::Matrix<double, 12, 12>;
+using Matrix2 = Eigen::Matrix<float, 2, 2>;
+using Matrix3 = Eigen::Matrix<float, 3, 3>;
+using Matrix4 = Eigen::Matrix<float, 4, 4>;
+using Matrix12 = Eigen::Matrix<float, 12, 12>;
 
-using Matrix13 = Eigen::Matrix<double, 1, 3>;
-using Matrix14 = Eigen::Matrix<double, 1, 4>;
-using Matrix112 = Eigen::Matrix<double, 1, 12>;
-using Matrix118 = Eigen::Matrix<double, 1, 18>;
-using Matrix34 = Eigen::Matrix<double, 3, 4>;
-using Matrix43 = Eigen::Matrix<double, 4, 3>;
-using Matrix64 = Eigen::Matrix<double, 6, 4>;
-using Matrix74 = Eigen::Matrix<double, 7, 4>;
-using MatrixN4 = Eigen::Matrix<double, Eigen::Dynamic, 4>;
-using Matrix3N = Eigen::Matrix<double, 3, Eigen::Dynamic>;
-using Matrix6N = Eigen::Matrix<double, 6, Eigen::Dynamic>;
-using MatrixN = Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic>;
+using Matrix13 = Eigen::Matrix<float, 1, 3>;
+using Matrix14 = Eigen::Matrix<float, 1, 4>;
+using Matrix112 = Eigen::Matrix<float, 1, 12>;
+using Matrix118 = Eigen::Matrix<float, 1, 18>;
+using Matrix34 = Eigen::Matrix<float, 3, 4>;
+using Matrix43 = Eigen::Matrix<float, 4, 3>;
+using Matrix64 = Eigen::Matrix<float, 6, 4>;
+using Matrix74 = Eigen::Matrix<float, 7, 4>;
+using MatrixN4 = Eigen::Matrix<float, Eigen::Dynamic, 4>;
+using Matrix3N = Eigen::Matrix<float, 3, Eigen::Dynamic>;
+using Matrix6N = Eigen::Matrix<float, 6, Eigen::Dynamic>;
+using MatrixN = Eigen::Matrix<float, Eigen::Dynamic, Eigen::Dynamic>;
 
 #endif  // TYPES_H_INCLUDED
--- cpuMLP/cMLP2.hpp
+++ cpuMLP/cMLP2.hpp
@@ -10,6 +10,7 @@
 //////////////////////////////////////////////////////////////////////////////////////////////////
 
 #include "Types.h"
+// #include "pinocchio/math/rpy.hpp"
 #include "cpuMLP.hpp"
 
 class cMLP2 {
@@ -36,14 +37,20 @@ class cMLP2 {
   /// \param[in] estFileName Name of file that contains estimation parameters
   ///
   ////////////////////////////////////////////////////////////////////////////////////////////////
-  void initialize(std::string polDirName, std::string estFileName);
+  void initialize(std::string polDirName, std::string estFileName, Vector12 q_init);
 
   ////////////////////////////////////////////////////////////////////////////////////////////////
   ///
   /// \brief  Forward pass
   ///
   ////////////////////////////////////////////////////////////////////////////////////////////////
-  void forward();
+  Vector12 forward();
+
+  void update_observation(Vector12 pos, Vector12 vel, Vector3 ori, Vector3 gyro);
+
+  void update_history(Vector12 pos, Vector12 vel);
+
+  Matrix3 rpyToMatrix(float r, float p, float y);
 
  private:
 
@@ -54,13 +61,13 @@ class cMLP2 {
   MLP_2<123, 3> state_estimator_;
 
   // Normalization of the input of the control policy
-  VectorN obs_mean_;
-  VectorN obs_var_;
+  Vector132 obs_mean_;
+  Vector132 obs_var_;
 
   // History buffers
   const int num_history_stack_ = 6;
   MatrixN q_pos_error_hist_;
-  MatrixN qd_hist;
+  MatrixN qd_hist_;
   Vector12 previous_action_;
   Vector12 preprevious_action_;
 
@@ -68,29 +75,35 @@ class cMLP2 {
   Vector12 P_;
   Vector12 D_;
   Vector12 pTarget12_;
+  Vector12 q_init_;
   Vector3 vel_command_;
-  VectorN state_est_obs_;
-  VectorN obs_;
+  Vector123 state_est_obs_;
+  Vector132 obs_;
+  Vector132 bound_;
+  Vector12 bound_pi_;
 
 };
 
 cMLP2::cMLP2()
-    : obs_mean_(VectorN::Zero(132)),
-      obs_var_(VectorN::Zero(132)),
+    : obs_mean_(Vector132::Zero()),
+      obs_var_(Vector132::Zero()),
       q_pos_error_hist_(MatrixN::Zero(num_history_stack_, 12)),
-      qd_hist(MatrixN::Zero(num_history_stack_, 12)),
+      qd_hist_(MatrixN::Zero(num_history_stack_, 12)),
       previous_action_(Vector12::Zero()),
       preprevious_action_(Vector12::Zero()),
       P_(Vector12::Zero()),
       D_(Vector12::Zero()),
       pTarget12_(Vector12::Zero()),
+      q_init_(Vector12::Zero()),
       vel_command_(Vector3::Zero()),
-      state_est_obs_(VectorN::Zero(123)),
-      obs_(VectorN::Zero(132)) {
+      state_est_obs_(Eigen::Matrix<float, 123, 1>::Zero()),
+      obs_(Vector132::Zero()),
+      bound_(Vector132::Ones() * 10.0f),
+      bound_pi_(Vector12::Ones() * 3.1415f) {
   // Empty
 }
 
-void cMLP2::initialize(std::string polDirName, std::string estFileName) {
+void cMLP2::initialize(std::string polDirName, std::string estFileName, Vector12 q_init) {
 
   // Control policy
   policy_.updateParamFromTxt(polDirName + "");
@@ -104,27 +117,101 @@ void cMLP2::initialize(std::string polDirName, std::string estFileName) {
   obsMean_file.open("mean.csv");
   obsVariance_file.open("var.csv");
   if(obsMean_file.is_open()) {
-    for(int i = 0; i < _obsMean.size(); i++){
+    for(int i = 0; i < obs_mean_.size(); i++){
       std::getline(obsMean_file, in_line);
-      _obsMean(i) = std::stod(in_line);
+      obs_mean_(i) = std::stof(in_line);
     }   
     obsMean_file.close();
   }
   if(obsVariance_file.is_open()) {
-    for(int i = 0; i < _obsVar.size(); i++){
+    for(int i = 0; i < obs_var_.size(); i++){
       std::getline(obsVariance_file, in_line);
-      _obsVar(i) = std::stod(in_line);
+      obs_var_(i) = std::stof(in_line);
     }
     obsVariance_file.close();   
   }
 
   // Control gains
-  P_ = (Vector3(3.0, 3.0, 3.0)).replicate<4, 1>();
-  D_ = (Vector3(0.2, 0.2, 0.2)).replicate<4, 1>();
+  P_ = (Vector3(3.0f, 3.0f, 3.0f)).replicate<4, 1>();
+  D_ = (Vector3(0.2f, 0.2f, 0.2f)).replicate<4, 1>();
+
+  // Initial position
+  q_init_ = q_init;
+  previous_action_ = q_init;
+  preprevious_action_ = q_init;
 
 }
 
-void cMLP2::forward() { 
-  std::cout << "Test" << std::endl;
-  std::cout << P_ << std::endl;
+Vector12 cMLP2::forward() {
+
+  obs_ = ((obs_ - obs_mean_).array() / (obs_var_ + .1E-4f * Vector132::Ones()).cwiseSqrt().array()).matrix();
+  obs_ = obs_.cwiseMax(-bound_).cwiseMin(bound_);
+
+  pTarget12_ = q_init_ + 0.3f * policy_.forward(obs_).cwiseMax(-bound_pi_).cwiseMin(bound_pi_);
+
+  return pTarget12_;
+}
+
+void cMLP2::update_observation(Vector12 pos, Vector12 vel, Vector3 ori, Vector3 gyro) {
+  // Update the history
+  update_history(pos, vel);
+
+  // Update observation vectors
+  state_est_obs_.head(3) = rpyToMatrix(ori(0, 0), ori(1, 0), ori(2, 0)).row(2);
+  state_est_obs_.segment<12>(3 + 12 * 0) = pos;
+  state_est_obs_.segment<12>(3 + 12 * 1) = vel;
+  state_est_obs_.segment<12>(3 + 12 * 2) = previous_action_;
+  state_est_obs_.segment<12>(3 + 12 * 3) = preprevious_action_;
+  state_est_obs_.segment<12>(3 + 12 * 4) = q_pos_error_hist_.row(0);
+  state_est_obs_.segment<12>(3 + 12 * 5) = qd_hist_.row(0);
+  state_est_obs_.segment<12>(3 + 12 * 6) = q_pos_error_hist_.row(2);
+  state_est_obs_.segment<12>(3 + 12 * 7) = qd_hist_.row(2);
+  state_est_obs_.segment<12>(3 + 12 * 8) = q_pos_error_hist_.row(4);
+  state_est_obs_.segment<12>(3 + 12 * 9) = qd_hist_.row(4);
+
+  obs_.head(3) = rpyToMatrix(ori(0, 0), ori(1, 0), ori(2, 0)).row(2);
+  obs_.segment<3>(3) = state_estimator_.forward(state_est_obs_).head(3);
+  obs_.segment<3>(6) = gyro;
+  obs_.segment<3>(9) = vel_command_;
+  obs_.segment<12>(12) = pos;
+  obs_.segment<12>(12 + 12 * 1) = vel;
+  obs_.segment<12>(12 + 12 * 2) = previous_action_;
+  obs_.segment<12>(12 + 12 * 3) = preprevious_action_;
+  obs_.segment<12>(12 + 12 * 4) = qd_hist_.row(0);
+  obs_.segment<12>(12 + 12 * 5) = q_pos_error_hist_.row(2);
+  obs_.segment<12>(12 + 12 * 6) = qd_hist_.row(2);
+  obs_.segment<12>(12 + 12 * 7) = q_pos_error_hist_.row(4);
+  obs_.segment<12>(12 + 12 * 8) = qd_hist_.row(4);
+
+}
+
+void cMLP2::update_history(Vector12 pos, Vector12 vel) {
+  // Age pos error history
+  for (int index = 1; index < q_pos_error_hist_.rows(); index++) {
+    q_pos_error_hist_.row(index - 1).swap(q_pos_error_hist_.row(index));
+  }
+
+  // Insert a new line at the end of the pos error history
+  q_pos_error_hist_.row(q_pos_error_hist_.rows() - 1) = pTarget12_ - pos;
+
+  // Age vel history
+  for (int index = 1; index < qd_hist_.rows(); index++) {
+    qd_hist_.row(index - 1).swap(qd_hist_.row(index));
+  }
+
+  // Insert a new line at the end of the vel history
+  qd_hist_.row(qd_hist_.rows() - 1) = vel;
+
+  // Remember previous actions
+  preprevious_action_ = previous_action_;
+  previous_action_ = pTarget12_;
+
+}
+
+Matrix3 cMLP2::rpyToMatrix(float r, float p, float y) {
+  typedef Eigen::AngleAxis<float> AngleAxis;
+  return (AngleAxis(y, Vector3::UnitZ())
+          * AngleAxis(p, Vector3::UnitY())
+          * AngleAxis(r, Vector3::UnitX())
+          ).toRotationMatrix();
 }
