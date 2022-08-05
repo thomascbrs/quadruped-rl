d42fb13484cab61c3c0d2656fdb3e9722b267fed
odri <odri@furano.laas.fr>
2022-08-05 10:11:01 +0200

    Clean Interface.cpp

--- cpuMLP/Interface.cpp
+++ cpuMLP/Interface.cpp
@@ -1,18 +1,1 @@
-#include "cMLP2.hpp"
-
-/*
-cMLP2::cMLP2()
-    : a_(0) {
-  // Empty
-}
-
-void cMLP2::initialize() {
-  
-  create_walk();
-}
-
-void cMLP2::create_walk() {
-  
-  std::cout << "Test " << a_ << std::endl;
-}
-*/
+#include "Interface.hpp"
